5d99c195a6801e6909066d15fe34b4671ae7c4f1
Maxut <55204645+SphaleRud@users.noreply.github.com>
2026-01-20 20:23:44 +0700

    Недостаток метода
    Метод который ты используешь, также использует число 0

--- FoodDelivery/restaurants/models.py
+++ FoodDelivery/restaurants/models.py
@@ -53,8 +53,9 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")  # отдельные позиции заказа
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+    quantity = models.PositiveIntegerField(default=1) #В метод входит также число 0, хотя в заказе не возможно
 
     def __str__(self):
 
         return f"{self.dish.name} x {self.quantity}"
+
